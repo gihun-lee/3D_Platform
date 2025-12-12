7ba6e4ac209d9a377a95ec008dd930b07596c99c
Gihun Lee <ghlee@hyvision.co.kr>
2025-12-12 18:38:20 +0900

    Node fileter parameter remove

--- src/VPP.App/obj/Debug/net8.0-windows/App.g.i.cs
+++ src/VPP.App/obj/Debug/net8.0-windows/App.g.i.cs
@@ -46,7 +46,7 @@ namespace VPP.App {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "10.0.0.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "10.0.1.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
@@ -58,7 +58,7 @@ namespace VPP.App {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/VPP.App;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/VPP.App;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -72,7 +72,7 @@ namespace VPP.App {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "10.0.0.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "10.0.1.0")]
         public static void Main() {
             VPP.App.App app = new VPP.App.App();
             app.InitializeComponent();
--- src/VPP.App/obj/Debug/net8.0-windows/Views/MatrixEditWindow.g.i.cs
+++ src/VPP.App/obj/Debug/net8.0-windows/Views/MatrixEditWindow.g.i.cs
@@ -174,13 +174,13 @@ namespace VPP.App.Views {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "10.0.0.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "10.0.1.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/VPP.App;component/views/matrixeditwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/VPP.App;V1.0.0.0;component/views/matrixeditwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Views\MatrixEditWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -190,7 +190,7 @@ namespace VPP.App.Views {
         }
         
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "10.0.0.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "10.0.1.0")]
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
