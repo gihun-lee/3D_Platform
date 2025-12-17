c732380d698bcb50c7c2cbba6f4c314e51d887fe
gihun-lee <ghlee@hyvision.co.kr>
2025-12-17 09:33:02 +0900

    Revert "Add various measurement tool features"

--- src/VPP.Plugins.PointCloud/PointCloudPlugin.cs
+++ src/VPP.Plugins.PointCloud/PointCloudPlugin.cs
@@ -9,46 +9,16 @@ public class PointCloudPlugin : IPlugin
     public string Name => "Point Cloud Processing";
     public string Version => "1.0.0";
     public string Author => "VPP Team";
-    public string Description => "Point cloud import, ROI, detection, measurement, and inspection nodes";
+    public string Description => "Point cloud import, ROI, circle detection, and inspection nodes";
 
     public IReadOnlyList<Type> NodeTypes { get; } = new List<Type>
     {
-        // Import & Transform
         typeof(ImportPointCloudNode),
-        typeof(RigidTransformNode),
-
-        // ROI
         typeof(ROIDrawNode),
         typeof(ROIFilterNode),
-
-        // Detection (Geometry Fitting)
         typeof(CircleDetectionNode),
-        typeof(PlaneFittingNode),
-        typeof(SphereFittingNode),
-        typeof(CylinderFittingNode),
-        typeof(LineFittingNode),
-
-        // Basic Measurements
-        typeof(DistanceMeasurementNode),
-        typeof(AngleMeasurementNode),
-        typeof(HeightMeasurementNode),
-        typeof(BoundingBoxMeasurementNode),
-        typeof(CentroidMeasurementNode),
-        typeof(PointDensityNode),
-        typeof(SurfaceAreaNode),
-        typeof(PointToPlaneDistanceNode),
-
-        // GD&T Measurements
-        typeof(FlatnessMeasurementNode),
-        typeof(RoundnessMeasurementNode),
-        typeof(CylindricityMeasurementNode),
-        typeof(ParallelismMeasurementNode),
-        typeof(PerpendicularityMeasurementNode),
-        typeof(ConcentricityMeasurementNode),
-        typeof(CoaxialityMeasurementNode),
-
-        // Inspection
-        typeof(InspectionNode)
+        typeof(InspectionNode),
+        typeof(RigidTransformNode) // Added rigid transform node
     };
 
     public void Initialize() { }
